af28f49d0e6ba485de1b89770563adb922e0b702
mariehardy <mademoiselle.mph@gmail.com>
2020-08-22 17:05:49 +0200

    screenshots added

--- src/pages/Project2.js
+++ src/pages/Project2.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import pic1 from '../assets/images/IMG_8281-1.jpg';
-import pic2 from '../assets/images/allmyplantslanding.jpg';
-import pic3 from '../assets/images/monkandmonkey.jpg';
+import plantyPic1 from '../assets/images/allmyplants-01.png';
+import plantyPic2 from '../assets/images/allmyplants-02.png';
+import plantyPic3 from '../assets/images/allmyplants-03.png';
+import plantyPic4 from '../assets/images/allmyplants-04.png';
 
 import Layout from '../components/Layout';
 
@@ -15,7 +16,7 @@ const IndexPage = () => (
       <section className="wrapper style5">
         <div className="inner">
           <p>
-          Virtual garden to keep track of all the plants you have at home and their specific care requirements.
+          Virtual garden to keep track of the plants you have at home and their specific care requirements.
           <hr />
           Technologies: Node.js, Express, MongoDB, Javascript, HTML, CSS
           <br />
@@ -38,17 +39,11 @@ const IndexPage = () => (
       <section>
 					<div className="box alt">
 						<div className="row gtr-50 gtr-uniform">
-							<div className="col-12"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
-						</div>
+						<div className="col-12"><span className="image fit"><img src={plantyPic1} alt="" /></span></div>
+							<div className="col-4"><span className="image fit"><img src={plantyPic3} alt="" /></span></div>
+							<div className="col-4"><span className="image fit"><img src={plantyPic4} alt="" /></span></div>
+              <div className="col-4"><span className="image fit"><img src={plantyPic2} alt="" /></span></div>
+          	</div>
 					</div>
 			</section>
 
--- src/pages/Project3.js
+++ src/pages/Project3.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import pic1 from '../assets/images/IMG_8281-1.jpg';
-import pic2 from '../assets/images/allmyplantslanding.jpg';
-import pic3 from '../assets/images/monkandmonkey.jpg';
+import monkPic1 from '../assets/images/monk-01.png';
+import monkPic2 from '../assets/images/monk-02.png';
+import monkPic3 from '../assets/images/monk-03.png';
+import monkPic4 from '../assets/images/monk-04.png';
 
 import Layout from '../components/Layout';
 
@@ -15,8 +16,13 @@ const IndexPage = () => (
         <div className="inner">
           <p>
           Retro game in which you are a monk who must bring 
-          an offering to Buddha Keanu Reeves&mdash;watch out for the monkeys!     
+          an offering to Buddha Keanu Reeves&mdash;watch out for the monkeys!
+          </p>
+          <p>
+          Soundtrack a courtesy of Nirvana.    
+          </p> 
           <hr />
+          <p>
           Technologies: avascript, Canvas, HTML & CSS
           </p>
           <br />
@@ -34,17 +40,11 @@ const IndexPage = () => (
       <section>
 					<div className="box alt">
 						<div className="row gtr-50 gtr-uniform">
-							<div className="col-12"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
-							<div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
-						</div>
+						<div className="col-12"><span className="image fit"><img src={monkPic1} alt="" /></span></div>
+							<div className="col-4"><span className="image fit"><img src={monkPic2} alt="" /></span></div>
+							<div className="col-4"><span className="image fit"><img src={monkPic3} alt="" /></span></div>
+              <div className="col-4"><span className="image fit"><img src={monkPic4} alt="" /></span></div>
+    						</div>
 					</div>
 			</section>
 
